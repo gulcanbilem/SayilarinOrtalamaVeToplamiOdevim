3340eb29f5e3b2f22b27f12ab29079059ef6534e
gulcanbilem <113993110+gulcanbilem@users.noreply.github.com>
2023-01-01 12:38:46 +0300

    Add files via upload

---
+++ SayiToplaOrtalamaÖdev/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayiToplaOrtalamaÖdev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.Write("Kaç Adet Sayi Gireceğinizi Yazınız: ");
+                int adet = Convert.ToInt32(Console.ReadLine());
+                int toplam = 0;
+                int sayi;
+                for (int i = 0; i <= adet; i++)
+                {
+                    Console.Write("Sayi giriniz: ");
+                    sayi = Convert.ToInt32(Console.ReadLine());
+                    toplam += sayi;
+                }
+                Console.WriteLine("Sayilarin Ortalaması = " + (toplam / adet));
+                Console.WriteLine("Sayilarin Toplamı = " + toplam);
+
+                Console.ReadLine();
+            }
+    }
+}
